f37f1811fbd2d1ee502ff6b190a2351ad6ffa216
Samuel Lima <samuel61lima@gmail.com>
2020-06-25 17:09:24 -0300

    estudando booleanos

---
+++ fase-02/introjs/booleanos.js
@@ -0,0 +1,86 @@
+const alunosDaTurmaA = [
+  {
+    nome: "Mayk", 
+    nota: 9.8,
+  },
+  { 
+    nome: "Diego", 
+    nota: 10,
+  },
+  {
+    nome: "Fulano", 
+    nota: 2,
+  },
+  { 
+    nome: "Beltrano", 
+    nota: 10,
+  },
+];
+
+const alunosDaTurmaB = [
+  {
+    nome: "Cleiton",
+    nota: 10,
+  },
+  {
+    nome: "Robson",
+    nota: 10,
+  },
+  {
+    nome: "Siclano",
+    nota: 0,
+  }
+];
+
+/* CALCULA A MÉDIA DAS NOTAS DOS ALUNOS DE UMA TURMA */
+function calculaMedia(alunos) {
+  let soma = 0;
+
+  for (let i = 0; i < alunos.length; i++) {
+    soma = soma + alunos[i].nota;
+  }
+
+  const media = soma / alunos.length;
+  return media;
+}
+
+// armazena as médias das turmas...
+const media1 = calculaMedia(alunosDaTurmaA);
+const media2 = calculaMedia(alunosDaTurmaB);
+
+function enviaMensagem(media, turma) {
+  if (media > 5) {
+    console.log(`A média da turma ${turma} foi de ${media.toFixed(2)}. Parabéns!`);
+  } else {
+    console.log(`A média da turma ${turma} foi menor que 5.`);
+  }
+}
+
+enviaMensagem(media1, "TurmaA");
+enviaMensagem(media2, "TurmaB");
+
+// marcar aluno como reprovado se nota for menor que 5
+// enviar como mensagem
+
+function marcarComoReprovado(aluno) {
+  aluno.reprovado = false;
+  if (aluno.nota < 5) {
+    aluno.reprovado = true;
+  }
+}
+
+marcarComoReprovado(alunosDaTurmaA)
+
+function enviarMensagemReprovado(aluno) {
+  if (aluno.reprovado) console.log(`O aluno ${aluno.nome} está reprovado!`)
+}
+
+function alunoEstahReprovado(alunos) {
+  for (let aluno of alunos) {
+    marcarComoReprovado(aluno);
+    enviarMensagemReprovado(aluno);
+  }
+}
+
+alunoEstahReprovado(alunosDaTurmaA)
+alunoEstahReprovado(alunosDaTurmaB)
