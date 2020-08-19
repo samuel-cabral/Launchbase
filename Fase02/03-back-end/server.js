bd74a182b289f61008eebb034ebd1bf9695c8050
Samuel Cabral <samuel61lima@gmail.com>
2020-08-19 19:00:30 -0300

    update: refatora aplicação

---
+++ Fase02/03-back-end/server.js
@@ -0,0 +1,30 @@
+// INITIALIZE EXPRESS AND NUNJUCKS
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+
+// EXPRESS USE STATIC FILES OF THE public FOLDER
+server.use(express.static('public'));
+
+server.set('view engine', 'html');
+
+// SET NUNJUCKS TO CONFIGURE views FOLDER
+nunjucks.configure('views', {
+  express:server
+});
+
+// ROUTES
+server.get('/', function (req, res) {
+  return res.render('index');
+});
+
+server.get('/portfolio', function (req, res) {
+  return res.render('portfolio');
+});
+
+// START SERVER
+server.listen(5000, function () {
+  console.log('🚀 server is running');
+});
+
