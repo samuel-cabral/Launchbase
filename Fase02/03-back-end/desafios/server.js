a664ad1cfce43d9361b738031c4ba4098871dd62
Samuel Cabral <samuel61lima@gmail.com>
2020-08-23 18:38:43 -0300

    update: dinamizanodo contéudo da página 'Sobre'

--- Fase02/03-back-end/desafios/server.js
+++ Fase02/03-back-end/desafios/server.js
@@ -3,7 +3,6 @@ const nunjucks = require('nunjucks');
 
 const server = express();
 const courses = require('./data');
-const technologies = require('./data');
 
 // server use static files from the public folder
 server.use(express.static('public'));
@@ -13,7 +12,8 @@ server.set('view engine', 'njk');
 
 // configure nunjucks
 nunjucks.configure('views', {
-  express: server
+  express: server,
+  autoescape: false
 });
 
 // routes
@@ -24,7 +24,27 @@ server.get('/', function (req, res) {
 
 // about page
 server.get('/about', function (req, res) {
-  return res.render('about', { items: technologies });
+  const about = {
+    title: 'Sobre',
+    name: 'Rocketseat',
+    logo_url: 'https://pbs.twimg.com/profile_images/1291682473592659968/sEorc6oh_400x400.jpg',
+    avatar_url: '<a target="_blank" href="https://rocketseat.com.br">Rocketseat</a>',
+    presentation: 'As melhores tecnologias em programação, direto ao ponto e do jeito certo.',
+    description: 'No meio de tanta informação e da quantidade de ferramentas que surgem todos os dias, você precisa de alguém que te leve na direção certa.',
+    
+    technologies: [
+      { title: 'NodeJS', logo: 'https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png'},
+      { title: 'ReactJS', logo: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'},
+      { title: 'React Native', logo: 'https://s3.amazonaws.com/media-p.slid.es/uploads/118447/images/2991881/reactpurple.png'},
+    ],
+    
+    links: [
+      { name: 'Github', url: 'https://github.com/rocketseat' },
+      { name: 'Instagram', url: 'https://instagram.com/rocketseat_oficial' },
+      { name: 'Facebook', url: 'http://fb.com/rocketseat' }
+    ]
+  }
+  return res.render('about', { about });
 });
 
 // if not found
